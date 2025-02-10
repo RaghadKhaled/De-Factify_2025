e41865710ea3b9e414b8eedcd47ba5627cdf4293
Raghad <raghadkhaled_24@hotmail.com>
2025-02-10 13:55:20 +0300

    Update model_NN.py

--- model_NN.py
+++ model_NN.py
@@ -1,1 +1,18 @@
+import os
+import clip
+import torch
+from torch import nn
+
+class RSID(nn.Module):
+    def __init__(self, img_encoder='ViT-L/14', num_class=2, text_option=1):
+        super().__init__()
+        self.clip_model, self.preprocess = clip.load(img_encoder, device='cpu', jit=False)
+
+    def forward(self, image_input):
+        image_feats = self.clip_model.encode_image(image_input)
+        return image_feats
+
+
+if __name__ == '__main__':
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
